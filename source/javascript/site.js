64b37ddbaa8a86ba79f43d577bad66a6e309d4d3
Aske Hansen <aske@deeco.dk>
2018-05-17 19:04:12 +0200

    -

--- source/javascript/site.js
+++ source/javascript/site.js
@@ -7,9 +7,9 @@ document.addEventListener('ready', () => {
   scrollSnapPolyfill()
 })
 
-import { Application } from "stimulus"
-import { definitionsFromContext } from "stimulus/webpack-helpers"
-
-const application = Application.start()
-const context = require.context("./controllers", true, /\.js$/)
-application.load(definitionsFromContext(context))
+// import { Application } from "stimulus"
+// import { definitionsFromContext } from "stimulus/webpack-helpers"
+//
+// const application = Application.start()
+// const context = require.context("./controllers", true, /\.js$/)
+// application.load(definitionsFromContext(context))
